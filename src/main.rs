2268d5ddbdf32a37821db78468a0a45c6e753d3d
Vincent Camus <camus.vincent@gmail.com>
2021-11-01 00:04:08 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ extern crate clap;
 extern crate unicode_normalization;
 
 use clap::{App, Arg};
-use radix_trie::Trie;
+use patricia_tree::PatriciaSet;
+use std::collections::HashSet;
 use std::fs::{metadata, File};
 use std::io::Read;
 use std::str;
@@ -11,49 +12,46 @@ use unicode_normalization::UnicodeNormalization;
 fn get_raw_dict(name: &str, resource_dir: &str) -> Vec<u8> {
     let dict_path_elts = vec![resource_dir.to_string(), name.to_string()];
     let dict_path = dict_path_elts.join("/");
-
     let mut f = File::open(&dict_path).expect("no file found");
     let metadata = metadata(&dict_path).expect("unable to read metadata");
     let mut buffer = vec![0; metadata.len() as usize];
     f.read(&mut buffer).expect("buffer overflow");
-
-    buffer
+    return buffer;
 }
 
 fn last_word(prefix: &Vec<u8>, spaces: &Vec<usize>) -> String {
-    let mut prefix_start = 0;
+    let mut i = 0;
     if spaces.len() > 0 {
-        prefix_start = spaces[spaces.len() - 1];
+        i = spaces[spaces.len() - 1];
     }
-    let new_cur_prefix = prefix[prefix_start..prefix.len()].to_vec();
-    let key = String::from(str::from_utf8(&new_cur_prefix).unwrap());
-
-    return key;
+    let new_cur_prefix = prefix[i..prefix.len()].to_vec();
+    return String::from(str::from_utf8(&new_cur_prefix).unwrap());
 }
 
-fn permute(
+fn anagrams(
     input: Vec<u8>,
     prefix: Vec<u8>,
     spaces: Vec<usize>,
-    trie: &Trie<String, bool>,
-    output: &mut Vec<String>,
+    trie: &PatriciaSet,
+    output: &mut Vec<HashSet<String>>,
 ) {
     if input.len() == 0 {
         let key = last_word(&prefix, &spaces);
-        match trie.get(&key) {
-            Some(_) => {
-                let mut result = prefix;
-                let mut i = 0;
-                for space in &spaces {
-                    result.insert(*space + i, ' ' as u8);
-                    i += 1;
-                }
-                let s = String::from(str::from_utf8(&result).unwrap());
-                println!("--> {}", s);
-                output.push(s);
-            }
-            None => {}
+        if !trie.contains(&key) {
+            return;
         }
+        let mut result = HashSet::new();
+        let mut i = 0;
+        for space in &spaces {
+            result.insert(String::from(str::from_utf8(&prefix[i..*space]).unwrap()));
+            i = *space;
+        }
+        result.insert(String::from(
+            str::from_utf8(&prefix[i..prefix.len()]).unwrap(),
+        ));
+
+        println!("--> {:?}", result);
+        output.push(result);
         return;
     }
 
@@ -67,31 +65,18 @@ fn permute(
         let mut new_prefix = prefix.to_vec();
         new_prefix.push(input[i]);
 
-        let rest_str = last_word(&rest, &Vec::new());
-        let new_prefix_str = last_word(&new_prefix, &Vec::new());
         let key = last_word(&new_prefix, &spaces);
-
-        // println!(            "{} prefix={} key={} spaces={:?} rest={}",            i, new_prefix_str, key, spaces, rest_str        );
-
-        match trie.get(&key) {
-            Some(_) => {
-                // Current prefix is a known word. Add a space and continue.
-                let mut new_spaces = spaces.to_vec();
-                new_spaces.push(new_prefix.len());
-                //println!("--> FOUND!");
-                permute(
-                    rest.to_vec(),
-                    new_prefix.to_vec(),
-                    new_spaces.to_vec(),
-                    trie,
-                    output,
-                );
-            }
-            None => {}
+        if trie.contains(&key) {
+            // Current prefix is a known word. Add a space and continue.
+            let mut new_spaces = spaces.to_vec();
+            new_spaces.push(new_prefix.len());
+            anagrams(rest.to_vec(), new_prefix.to_vec(), new_spaces, trie, output);
         }
 
-        permute(rest, new_prefix, spaces.to_vec(), trie, output);
         i += 1;
+
+        // Try also with a longer prefix.
+        anagrams(rest, new_prefix, spaces.to_vec(), trie, output);
     }
 }
 
@@ -119,7 +104,7 @@ fn main() {
     let input = matches.value_of("INPUT").unwrap().to_lowercase();
 
     let raw_dict_fr = get_raw_dict("fr.txt", res_dir);
-    let mut trie = Trie::new();
+    let mut trie = PatriciaSet::new();
 
     let mut i = 0;
     let mut start = 0;
@@ -128,7 +113,7 @@ fn main() {
             let u8_word = &raw_dict_fr[start..i];
             let word = str::from_utf8(u8_word).unwrap();
             let ascii_word: String = word.nfd().filter(char::is_ascii).collect();
-            trie.insert(ascii_word.to_lowercase(), true);
+            trie.insert(ascii_word.to_lowercase());
             i += 1;
             start = i;
             continue;
@@ -136,13 +121,26 @@ fn main() {
         i += 1;
     }
 
-    let input_vec = input.as_bytes().to_vec();
+    let mut input_vec = input.as_bytes().to_vec();
+    input_vec.sort();
     let mut outputs = Vec::new();
-    permute(input_vec, Vec::new(), Vec::new(), &trie, &mut outputs);
+    anagrams(input_vec, Vec::new(), Vec::new(), &trie, &mut outputs);
 
-    outputs.sort_by(|a, b| a.matches(' ').count().cmp(&b.matches(' ').count()));
-
+    let mut results = HashSet::new();
     for result in outputs {
+        let mut tmp = Vec::from_iter(result);
+        tmp.sort();
+        let s = tmp.join(" ");
+        results.insert(s);
+    }
+
+    let mut ordered_results = Vec::new();
+    for result in results {
+        ordered_results.push(result);
+    }
+    ordered_results.sort_by(|a, b| b.len().cmp(&a.len()));
+
+    for result in ordered_results {
         println!("{}", result);
     }
 }
